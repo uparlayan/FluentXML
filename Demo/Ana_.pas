b0f45021e535b8aa2f0702583371a883a64d8719
Uğur PARLAYAN <ugurparlayan@gmail.com>
2020-08-16 14:45:08 +0300

    Proje grubuna Demo için taslak bir proje eklendi.

---
+++ Demo/Ana_.pas
@@ -0,0 +1,24 @@
+unit Ana_;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TAna = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Ana: TAna;
+
+implementation
+
+{$R *.dfm}
+
+end.
---
+++ Demo/FluentXml_Demo.dpr
@@ -0,0 +1,15 @@
+program FluentXml_Demo;
+
+uses
+  Vcl.Forms,
+  Ana_ in 'Ana_.pas' {Ana},
+  FluentXML_ in '..\Src\FluentXML_.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TAna, Ana);
+  Application.Run;
+end.
